fd85545f9b6210e1ebe0c86c76f7def004665de9
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-07-13 10:40:02 -0600

    got random pokemon

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,20 @@ var app = angular.module('pokemonApp', []);
 
 app.controller('myController', ['$http', '$scope', function($http, $scope) {
   $scope.greeting = "Hello World!";
+  $scope.generatePokemon = function() {
+    var randomPokemon = Math.floor(Math.random() * 720)+1;
+
+    $http({
+      url: 'http://pokeapi.co/api/v2/pokemon/' +
+            randomPokemon + '/',
+      method: 'GET'
+    })
+    .then(function (result) {
+      console.log(result.data.sprites);
+      $scope.pokemon = result.data;
+      $scope.img = result.data.sprites.front_default;
+      // $scope.loadingName = false;
+    });
+  };
+
 }]);
